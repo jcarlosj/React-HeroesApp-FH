1b060d8ffdaab81f4fbf06ee3fd0816b853c707c
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-12-07 16:02:56 -0500

    [.] index/App: Preparando Proyecto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <section className="container mt-5 mb-5">
+            <h1>Heroes App</h1>
+            <hr />
+        </section>
+    );
 }
 
 export default App;
