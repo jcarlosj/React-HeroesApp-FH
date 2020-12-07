48f3e53a596c77269bdd0766cd330ab899411087
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-12-07 17:05:31 -0500

    [+] LoginPage: Redireciona al Home. Reemplaza entrada en el historial de navegacion

--- src/components/login/LoginPage.js
+++ src/components/login/LoginPage.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
 /** Functional Component */
-export const LoginPage = () => {
+export const LoginPage = ({ history }) => {
+
+    const handleLogin = () => {
+        history.replace( '/' );        //  Redirecciona a '/' y Reemplaza la entrada actual en la pila del historial, en nuestro caso '/login' ruta de este Page Component
+    }
+
     return (
         <section className="container mt-5 mb-5">
-            <h1>Login Page</h1>
+            <h1>Login</h1>
             <hr />
+
+            <button
+                className="btn btn-primary"
+                onClick={ handleLogin }
+            >Login</button>
+
         </section>
     )
 }
