d8db7dbb30416ef115f89a3ff4c0ddc496e97350
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-12-08 13:18:03 -0500

    [.] HeroPage: Define - Valida datos, Crea boton de retorno

--- src/components/heroes/HeroPage.js
+++ src/components/heroes/HeroPage.js
@@ -1,17 +1,84 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Redirect } from 'react-router-dom';
+
+/** Functions */
+import { getHeroById } from '../../selectors/getHeroById';
 
 /** Functional Component */
-export const HeroPage = () => {
+export const HeroPage = ({ history }) => {
 
-    const params = useParams();    //  Extrae los parametros que van por el URL
-    console.log( params );
+    const 
+        { hero_id } = useParams(),    //  Extrae los parametros que van por el URL
+        hero = getHeroById( hero_id );
+
+    if( ! hero ) {                    //  Valida si no existen datos del heroe
+        return <Redirect to="/" />;   //  Redirecciona al home
+    }
+
+    const 
+        { 
+            superhero,
+            publisher,
+            alter_ego,
+            first_appearance,
+            characters
+        } = hero;
+
+    const handleReturn = () => {
+
+        // console.log( history );
+        if( history.length <= 2 ) {
+            if( publisher === 'DC Comics' ) {
+                history.push( '/dc' );
+                return;
+            } 
+            else if( publisher === 'Marvel Comics' ) {
+                history.push( '/marvel' );
+                return;
+            }
+            
+            history.push( '/' );
+            return;
+        }
+
+        history.goBack();        //  Redirecciona a la pagina anterior
+    }
 
     return (
         <section className="container mt-5 mb-5">
-            <h1>Hero Page</h1>
-            <p>id: { params.hero_id }</p>
-            <hr />
+            
+            <div className="row">
+                <div className="col-12 col-md-4">
+                <img 
+                    src={ `../assets/images/${ hero_id }.jpg` } 
+                    className="img-thumbnail" 
+                    alt={ superhero } 
+                />
+                </div>
+                <div className="col-12 col-md-8">
+                    <h1>{ superhero }</h1>
+                    <hr />
+                    <ul className="list-group list-group-flush">
+                        <li className="list-group-item"><strong>Alter ego: </strong> { alter_ego }</li>
+                        <li className="list-group-item"><strong>Publisher: </strong> { publisher }</li>
+                        <li className="list-group-item"><strong>First Appearance: </strong> { first_appearance }</li>
+                    </ul>
+                    <br />
+                    
+                    <h5>Characters</h5>
+                    <hr />
+                    <ul className="list-group list-group-flush">
+                        <li className="list-group-item">{ characters }</li>
+                    </ul>
+
+                    <button
+                        className="btn btn-outline-success"
+                        onClick={ handleReturn }
+                    >Return</button>
+
+                </div>
+            </div>
+
         </section>
     )
 }
