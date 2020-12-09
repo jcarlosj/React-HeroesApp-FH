96346a93f0d2de95467d64871618a8c85c7aedd6
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-12-09 10:09:53 -0500

    [.] AuthContext/authReducer/types: Define Types, Context y Reducer para proveer datos de autenticacion a toda la app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { useReducer } from "react";
+
 /** Router */
 import { AppRouter } from "./routers/AppRouter";
 
+/** Reducer */
+import { authReducer } from "./auth/authReducer";
+
+/** Context */
+import { AuthContext } from "./auth/AuthContext";
+
+/** Obtiene localStorage o establece valores por defecto */
+const init = () => {
+    return JSON.parse( localStorage.getItem( 'user' ) ) || {
+        logged: false
+    };
+}
+
+/** Functional Component */
 const App = () => {
+
+    const [ user, dispatch ] = useReducer( authReducer, {}, init );     //  Implementa Reducer de Autenticacion
+
     return (
-        <AppRouter />
+        <AuthContext.Provider value={{      //  Hace disponible las propiedades y funciones del reducer en todo el árbol de componentes de la aplicación
+            user, 
+            dispatch
+        }}>
+            <AppRouter />
+        </AuthContext.Provider>
     );
 }
 
---
+++ src/auth/AuthContext.js
@@ -0,0 +1,4 @@
+import { createContext } from 'react';
+
+/** AuthContext: High Order Component */
+export const AuthContext = createContext();
