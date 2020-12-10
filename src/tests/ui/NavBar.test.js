aa89f8128f335703cfe190f61b3b45b3829116bc
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-12-10 16:04:27 -0500

    [.] NavBar.test: Define 'debe invocar handleLogout al pulsar el boton logout y usar history para redireccionar'

--- src/tests/ui/NavBar.test.js
+++ src/tests/ui/NavBar.test.js
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, Router } from 'react-router-dom';
 import { mount } from 'enzyme';
 
 /** Context */
@@ -8,7 +8,10 @@ import { AuthContext } from '../../auth/AuthContext';
 /** Components */
 import { NavBar } from '../../components/ui/NavBar';
 
-describe( 'Dashboard Router', () => {
+/** Types */
+import { types } from '../../types/types';
+
+describe( 'NavBar UI', () => {
 
     const 
         contextValue = {      
@@ -20,21 +23,46 @@ describe( 'Dashboard Router', () => {
             },                           
             dispatch: jest.fn()     //  Disparador de acciones de nuestro Context
         },
+        historyMock = {
+            push: jest.fn(),
+            location: {},
+            listen: jest.fn(),
+            createHref: jest.fn(),
+            replace: jest.fn()
+        },
         wrapper = mount( 
             <AuthContext.Provider value={       //  Hace disponible las propiedades y funciones del reducer en todo el árbol de componentes de la aplicación
                 contextValue    
             }>
                 <MemoryRouter>
-                    <NavBar /> 
+                    <Router history={ historyMock }>
+                        <NavBar /> 
+                    </Router>
                 </MemoryRouter>
             </AuthContext.Provider>
         );
+
+    afterEach( () => {
+        jest.clearAllMocks();
+    });
 
     test( 'debe mostrarse correctamente', () => { 
 
         /** Esta prueba evalua al componente hijo, para ello se debe usar 'mount' para montar el componente, en el momento esta funcion no es compatible con React 17 */
         expect( wrapper ).toMatchSnapshot();
         expect( wrapper.find( 'span.text-white' ).text().trim() ).toBe( 'Hola, Eva Sofia' );
+
+    } );
+
+    test( 'debe invocar handleLogout al pulsar el boton logout y usar history para redireccionar', () => { 
+
+        wrapper.find( 'button' ).prop( 'onClick' )();   //  Hace click en el boton logout
+
+        expect( contextValue.dispatch ).toHaveBeenCalledWith( {
+            type: types.logout,
+            payload: null
+        } );
+        expect( historyMock.replace ).toHaveBeenCalledWith( '/login' );
 
     } );
 
