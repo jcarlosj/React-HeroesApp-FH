31dd16e20c622d950e42141e90951b31cf8cf616
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-12-11 10:38:41 -0500

    [+] SearchPage.test: Define 'debe mostrar a Batman e input con el valor del queryString en la URL'

--- src/tests/components/search/SearchPage.test.js
+++ src/tests/components/search/SearchPage.test.js
@@ -25,4 +25,21 @@ describe( 'SearchPage Component', () => {
 
     } );
 
+    test( 'debe mostrar a Batman e input con el valor del queryString en la URL', () => { 
+
+        /** Esta prueba evalua al componente hijo, para ello se debe usar 'mount' para montar el componente, en el momento esta funcion no es compatible con React 17 */
+        const wrapper = mount(
+            <MemoryRouter initialEntries={[ '/search?q=batman' ]}>
+                <Route
+                    path="/search"
+                    component={ SearchPage }
+                />
+            </MemoryRouter>
+        );
+
+        expect( wrapper.find( 'input' ).prop( 'value' ) ).toBe( 'batman' );
+        expect( wrapper ).toMatchSnapshot();
+
+    } );
+
 } );
