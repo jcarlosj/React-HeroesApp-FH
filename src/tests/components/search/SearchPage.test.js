e74a2dd4a81b2f269f1428124d1135226a7f3610
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-12-11 10:31:01 -0500

    [.] SearchPage.test: Define 'debe mostrarse correctamente con sus valores por defecto'

---
+++ src/tests/components/search/SearchPage.test.js
@@ -0,0 +1,28 @@
+import '@testing-library/jest-dom';
+import { mount } from 'enzyme';
+import { MemoryRouter, Route } from 'react-router-dom';
+
+/** Components */
+import { SearchPage } from '../../../components/search/SearchPage';
+
+
+describe( 'SearchPage Component', () => {
+
+    test( 'debe mostrarse correctamente con sus valores por defecto', () => { 
+
+        /** Esta prueba evalua al componente hijo, para ello se debe usar 'mount' para montar el componente, en el momento esta funcion no es compatible con React 17 */
+        const wrapper = mount(
+            <MemoryRouter initialEntries={[ '/search' ]}>
+                <Route
+                    path="/search"
+                    component={ SearchPage }
+                />
+            </MemoryRouter>
+        );
+
+        expect( wrapper ).toMatchSnapshot();
+        expect( wrapper.find( 'p.alert-info' ).text().trim() ).toBe( 'Search a hero...' );
+
+    } );
+
+} );
